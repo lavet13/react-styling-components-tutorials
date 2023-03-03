20fe92171cd34506799b76e3a1417c298789d48c
lavet13 <lavet13@mail.ru>
2023-03-03 20:01:20 +0300

    styled-components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,7 @@
 // import styled from 'styled-components';
-// import './Button.css';
 
+import React from 'react';
+// import './Button.css';
 import styles from './Button.module.css'; // this is how you have to import a css file if you wanna use CSS modules
 
 /*
@@ -29,6 +30,23 @@ const Button = styled.button`
     }
 `;
 */
+
+// CSS Modules
+// styles is an object and in that object, you'll have every class you defined in your file as a property.
+// So If I added a button class in the CSS file I now have the button property in styles object.
+// Build process under the hood. It is takes those CSS classes and a CSS file and basically changes those
+// class names to be unique. That's the core thing it does. For every component it changes the class names
+// off the classes you're importing here. So off the CSS file you were importing to be unique.
+// So if you import from the CSS file here in the button component, it will create unique classes, unique
+// versions of all those styles and classes here for this component. And for data it generates brand new
+// class names. Inside of these class names, your styles will be so it will just keep your styles.
+// It will not touch those, but wrapped into this new class name.
+// With CSS modules concept ensures that the CSS styles we set up in a CSS file are scoped to the component
+// we import this file into. For that we need to work with CSS classes here because we then access those classes
+// as properties on the imported styles object. This is how we basically make a connection between those dynamically
+// generated class names, which we as a developer don't know in advance and our components here.
+// That's actually the best of both worlds. We have our styles set up in CSS file but there's still scoped to the components
+// where we use them.
 
 const Button = ({ type, onClick, children }) => {
     return (
