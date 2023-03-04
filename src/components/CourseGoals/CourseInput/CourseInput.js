1631e72b2ab21f799631413dd2e29ef85d4673a4
lavet13 <lavet13@mail.ru>
2023-03-04 07:54:36 +0300

    dynamic styles with css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,54 +1,54 @@
 import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
+// import styled, { css } from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-    margin: 0.5rem 0;
+// const FormControl = styled.div`
+//     margin: 0.5rem 0;
 
-    & label {
-        font-weight: bold;
-        display: block;
-        margin-bottom: 0.5rem;
-        ${({ invalid }) =>
-            invalid &&
-            css`
-                color: red;
-            `}
-    }
+//     & label {
+//         font-weight: bold;
+//         display: block;
+//         margin-bottom: 0.5rem;
+//         ${({ invalid }) =>
+//             invalid &&
+//             css`
+//                 color: red;
+//             `}
+//     }
 
-    & input {
-        display: block;
-        width: 100%;
-        border: 1px solid ${({ invalid }) => (invalid ? 'red' : '#ccc')};
-        font: inherit;
-        line-height: 1.5rem;
-        padding: 0 0.25rem;
-        ${({ invalid }) =>
-            invalid &&
-            css`
-                color: white;
-                background-color: #ffd7d7;
-            `}
-    }
+//     & input {
+//         display: block;
+//         width: 100%;
+//         border: 1px solid ${({ invalid }) => (invalid ? 'red' : '#ccc')};
+//         font: inherit;
+//         line-height: 1.5rem;
+//         padding: 0 0.25rem;
+//         ${({ invalid }) =>
+//             invalid &&
+//             css`
+//                 color: white;
+//                 background-color: #ffd7d7;
+//             `}
+//     }
 
-    & input:focus {
-        outline: none;
-        background: #fad0ec;
-        border-color: #8b005d;
-    }
+//     & input:focus {
+//         outline: none;
+//         background: #fad0ec;
+//         border-color: #8b005d;
+//     }
 
-    /* &.invalid input {
-        border-color: #ff0f0faa;
-        color: white;
-        background-color: red;
-    }
+//     /* &.invalid input {
+//         border-color: #ff0f0faa;
+//         color: white;
+//         background-color: red;
+//     }
 
-    &.invalid label {
-        color: red;
-    } */
-`;
+//     &.invalid label {
+//         color: red;
+//     } */
+// `;
 
 const CourseInput = ({ onAddGoal }) => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -75,14 +75,18 @@ const CourseInput = ({ onAddGoal }) => {
     return (
         <form onSubmit={formSubmitHandler}>
             {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`.trim()}> */}
-            <FormControl invalid={!isValid}>
+            <div
+                className={`${styles['form-control']} ${
+                    !isValid ? styles.invalid : ''
+                }`.trim()}
+            >
                 <label>Course Goal</label>
                 <input
                     type="text"
                     onChange={goalInputChangeHandler}
                     value={enteredValue}
                 />
-            </FormControl>
+            </div>
             {/* </div> */}
             <Button type="submit">Add Goal</Button>
         </form>
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -32,21 +32,35 @@ const Button = styled.button`
 */
 
 // CSS Modules
-// styles is an object and in that object, you'll have every class you defined in your file as a property.
+// Wherever you like styled components or not is absolute personal thing, there is no wrong or right here.
+// He prefers CSS in the CSS files. He likes the separation of CSS and JavaScript. He likes having clean JavaScript files.
+// And he simply likes default CSS. You can absolutely stick to the CSS approach we used at the beginning where
+// you had global non scoped styles and you as a developer had to make sure you're not accidentally reusing class names.
+// You're not accidentally affecting the styles of other components. This is definitely one route you can take.
+// You can also however take CSS to the next level with a feature called CSS Modules. CSS Modules is a feature which is only
+// available in projects that are configured to support it, because it needs a code transformation that needs to be done.
+// before your code runs in the browser. Now, the good thing is the react project is created with create react app which we
+// used are already configured to support CSS Modules.
+// When you importing CSS file into a component you should name your CSS file with ".module.css". This is basically a signal
+// to the underlying compilation process to transform the code so that CSS Modules work.
+
+// Styles is an object and in that object, you'll have every class you defined in your file as a property.
 // So If I added a button class in the CSS file I now have the button property in styles object.
-// Build process under the hood. It is takes those CSS classes and a CSS file and basically changes those
+// Build process under the hood. It is takes those CSS classes in the CSS file and basically changes those
 // class names to be unique. That's the core thing it does. For every component it changes the class names
-// off the classes you're importing here. So off the CSS file you were importing to be unique.
+// of the classes you're importing here. So of the CSS file you were importing to be unique.
 // So if you import from the CSS file here in the button component, it will create unique classes, unique
-// versions of all those styles and classes here for this component. And for data it generates brand new
+// versions of all those styles and classes here for this component. And for that it generates brand new
 // class names. Inside of these class names, your styles will be so it will just keep your styles.
-// It will not touch those, but wrapped into this new class name.
+// It will not touch those, but wrapped into new class name.
 // With CSS modules concept ensures that the CSS styles we set up in a CSS file are scoped to the component
 // we import this file into. For that we need to work with CSS classes here because we then access those classes
-// as properties on the imported styles object. This is how we basically make a connection between those dynamically
+// as properties on the imported styles object. This is how we basically make a connection between these dynamically
 // generated class names, which we as a developer don't know in advance and our components here.
 // That's actually the best of both worlds. We have our styles set up in CSS file but there's still scoped to the components
 // where we use them.
+// Now, ultimately, it's absolutely up to you which approach you prefer. I showed you the CSS only, the styled components,
+// and the CSS modules approach and I mentioned the different pros and cons. And you can simply use the approach you like.
 
 const Button = ({ type, onClick, children }) => {
     return (
